45a409cb07983a049d9507af2a4f8934d5899e19
serdaryilmaz14 <116540924+serdaryilmaz14@users.noreply.github.com>
2023-08-19 12:09:02 +0300

    Create Main.java
    Sabit Değişken

---
+++ degiskenler2/Main.java
@@ -0,0 +1,14 @@
+package degiskenler2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14159;
+		double yaricap = 7.0;
+		double alan = PI * yaricap * yaricap;
+		System.out.print("Alan: " + alan);
+		
+	}
+
+}
