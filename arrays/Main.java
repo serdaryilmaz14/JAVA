8881984b8911e1abf3170a91d831a054f420b7d1
serdaryilmaz14 <116540924+serdaryilmaz14@users.noreply.github.com>
2023-08-19 12:27:55 +0300

    Create Main.java
    Diziler

---
+++ arrays/Main.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] meyveler = {"Elma", "Armut", "Portakal", "Muz"};
+		int meyveSayisi = meyveler.length;
+		System.out.println("Meyve Sayisi: " + meyveSayisi);
+		System.out.println("1. Meyve: " + meyveler[0]);
+		System.out.println("2. Meyve: " + meyveler[1]);
+		System.out.println("3. Meyve: " + meyveler[2]);
+		System.out.println("4. Meyve: " + meyveler[3]);
+	}
+
+}
