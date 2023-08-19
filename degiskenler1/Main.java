dd65861a6ce32411bcfa41039e8044430f314289
serdaryilmaz14 <116540924+serdaryilmaz14@users.noreply.github.com>
2023-08-19 12:01:45 +0300

    Create Main.java
    Sayısal Değişkenler

---
+++ degiskenler1/Main.java
@@ -0,0 +1,18 @@
+package degiskenler1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String isim = "Mehmet";
+		int yas = 22;
+		double ortalamaNot = 86.7;
+		long cekilenPara = 2500000;
+		
+		System.out.println("Isim: " + isim);
+		System.out.println("Yas: " + yas);
+		System.out.println("Ortalama Not: " + ortalamaNot);
+		System.out.println("Cekilen Para: " + cekilenPara);
+	}
+
+}
