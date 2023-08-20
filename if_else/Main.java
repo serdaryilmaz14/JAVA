df150a9a0aee764882a7905ced487d485721484f
serdaryilmaz14 <116540924+serdaryilmaz14@users.noreply.github.com>
2023-08-20 12:00:00 +0300

    Create Main.java
    İf - Else

---
+++ if_else/Main.java
@@ -0,0 +1,19 @@
+package if_else;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sayi = 10;
+		if (sayi < 0) {
+			System.out.println("Sayi negatif");
+		}else if (sayi > 0) {
+			System.out.println("Sayi pozitif");
+		}else{
+			System.out.println("Sayi 0'a esittir");
+		
+		}
+	}
+
+}
