7e78e807aa8b2932202b63b7565163645fc2b4ac
serdaryilmaz14 <116540924+serdaryilmaz14@users.noreply.github.com>
2023-08-20 12:47:45 +0300

    Create Main.java
    While Döngüsü

---
+++ whileLoop/Main.java
@@ -0,0 +1,20 @@
+package whileLoop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1' den 10'a kadar olan sayıların karesi
+	
+		int j = 1;
+
+        while (j <= 10) {
+            int karesi = j * j;
+            System.out.println(karesi);
+            j++;
+			
+		}
+	
+	}
+
+}
