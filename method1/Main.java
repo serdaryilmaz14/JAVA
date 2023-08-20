ea72a9d40e3cb8bd32eb0bfa68e1c9d76aedd5d2
serdaryilmaz14 <116540924+serdaryilmaz14@users.noreply.github.com>
2023-08-20 13:14:04 +0300

    Create Main.java
    Method Örneği

---
+++ method1/Main.java
@@ -0,0 +1,19 @@
+package method1;
+
+public class Main {
+	
+	public static void selamla(){
+		System.out.println("Merhaba!");
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Dönüş değeri olmayan metod örneği
+		
+		selamla();
+		
+		
+	}
+
+}
